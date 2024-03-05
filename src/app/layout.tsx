26b7a531db945e19e057326940abd0940f83eece
Elluzion <fynnruebenach@gmail.com>
2024-03-06 00:48:45 +0100

    analytics: Add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 
 import { mainFont, monoFont } from "@/components/ui/providers/font-provider";
 import { ThemeProvider } from "@/components/ui/providers/theme-provider";
 import PageHeader from "@/components/ui/unique/PageHeader";
+import CursorBacklight from "@/components/ui/unique/cursor-backlight";
 
 import "./globals.css";
-import CursorBacklight from "@/components/ui/unique/cursor-backlight";
 
 /**
  * The metadata tags of the page for SEO
@@ -25,18 +26,20 @@ export default function RootLayout({
       <body
         className={`${mainFont.variable} ${monoFont.variable} flex flex-col`}
       >
-        <CursorBacklight />
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
+          <CursorBacklight />
           <PageHeader />
           <div className="w-full mt-16 pt-16 px-4 max-w-[700px] md:px-0 self-center transition-all">
             {children}
           </div>
         </ThemeProvider>
+        {/* Vercel analytics */}
+        <Analytics />
       </body>
     </html>
   );
