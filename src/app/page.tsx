06435f6edeb769d29c7bbc46854c40ee1cb9b448
Elluzion <fynnruebenach@gmail.com>
2024-03-05 22:14:06 +0100

    Remove bright mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="prose dark:prose-invert prose-zinc flex flex-col max-w-full">
+    <main className="prose prose-invert prose-zinc flex flex-col max-w-full">
       <h4>elluzion</h4>
       <p>
         I am Fynn, a music producer, software developer and overall creative
