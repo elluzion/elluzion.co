c5845587914d5827ef196bbe872687644dc51298
Elluzion <fynnruebenach@gmail.com>
2024-03-11 21:19:46 +0100

    💫  PlatformButton: Add animation

--- src/app/music/[songWrittenId]/PlatformButton.tsx
+++ src/app/music/[songWrittenId]/PlatformButton.tsx
@@ -1,23 +1,26 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import React from "react";
 
 import { Platforms } from "@/lib/songs/platforms";
-import { SiSpotify } from "@icons-pack/react-simple-icons";
 
 import { pascalCase } from "@/lib/utils";
 
 import Icon from "@mdi/react";
 import { mdiPlay } from "@mdi/js";
 
+import { motion } from "framer-motion";
+
 interface PlatformButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   platform: string | null;
+  entry: number;
 }
-
 export const PlatformButton = React.forwardRef<
   HTMLButtonElement,
   PlatformButtonProps
->(({ className, platform, ...props }, ref) => {
+>(({ className, platform, entry, ...props }, ref) => {
   const platformData = Platforms.find((x) => x.id == platform);
 
   const accent = platformData?.accentColour || "#242424";
@@ -26,17 +29,23 @@ export const PlatformButton = React.forwardRef<
   const darkForeground = platformData?.darkForeground || false;
 
   return (
-    <Button
-      className={`h-14 w-full flex gap-2 text-md hover:scale-[.98] transition-[transform,filter] hover:brightness-110 ${
-        darkForeground ? "text-zinc-950" : "text-white"
-      }`}
-      style={{
-        backgroundColor: accent,
-      }}
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1, transition: { delay: 0.1 * entry } }}
+      className="w-full h-14"
     >
-      {icon}
-      {name}
-    </Button>
+      <Button
+        className={`h-14 w-full flex gap-2 text-md hover:scale-[.98] transition-[transform,filter] hover:brightness-110 ${
+          darkForeground ? "text-zinc-950" : "text-white"
+        }`}
+        style={{
+          backgroundColor: accent,
+        }}
+      >
+        {icon}
+        {name}
+      </Button>
+    </motion.div>
   );
 });
 PlatformButton.displayName = "PlatformButton";
