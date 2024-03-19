3280359d6428e882258d2932dd2ebdb7b42a9f15
Elluzion <fynnruebenach@gmail.com>
2024-03-19 01:24:21 +0100

    🩹  formSchema: Define max decimals for numeric values

--- src/app/add/formSchema.ts
+++ src/app/add/formSchema.ts
@@ -7,13 +7,13 @@ export const formSchema = z.object({
   writtenId: z.string().min(1).max(20),
   artists: z.array(
     z.object({
-      id: z.optional(z.coerce.number()),
+      id: z.optional(z.coerce.number().multipleOf(1)),
       name: z.string(),
     })
   ),
   // second page
   coverUrl: z.string().url().min(1),
-  bpm: z.optional(z.coerce.number().min(0)),
+  bpm: z.optional(z.coerce.number().min(0).multipleOf(0.01)),
   genre: z.string().max(15),
   releaseType: z.string().max(15),
   key: z.optional(z.string().max(15)),
