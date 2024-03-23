074f629bc86c8a0a8a912db710204e380fcd8a3b
Elluzion <fynnruebenach@gmail.com>
2024-03-23 21:23:07 +0100

    🎨 Remove redundant typing Its already set through the getSong function's return values

--- src/app/[songId]/page.tsx
+++ src/app/[songId]/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
-import { Song, getSong } from "@/lib/songs/song-parser";
+import { getSong } from "@/lib/songs/song-parser";
 import { SongDisplay } from "./_components/SongDisplay";
 import { Suspense } from "react";
 import { LoadingContainer } from "@/components/loading-container";
@@ -10,7 +10,7 @@ export default async function SongPage({
   params: { songId: string };
 }) {
   // gather all info from the database
-  const song: Song | null = await getSong(params.songId);
+  const song = await getSong(params.songId);
   // return to home page if track not found
   if (!song) redirect("/");
 
