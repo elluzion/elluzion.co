7eeddb1f8dfafaf6ce64123928f50a25d647eee5
Elluzion <fynnruebenach@gmail.com>
2024-04-22 00:15:07 +0200

    🎨 Move DataCard to own component file

--- src/app/tools/analyzer/analyzer-screen.tsx
+++ src/app/tools/analyzer/analyzer-screen.tsx
@@ -2,18 +2,12 @@
 
 import { InfoCard } from "@/components/info-card";
 import { useToast } from "@/components/use-toast";
-import {
-  mdiCheck,
-  mdiContentCopy,
-  mdiPiano,
-  mdiSpeaker,
-  mdiSpeedometer,
-} from "@mdi/js";
-import Icon from "@mdi/react";
+import { mdiPiano, mdiSpeaker, mdiSpeedometer } from "@mdi/js";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import { useState } from "react";
 //@ts-ignore
+import DataCard from "./_components/data-card";
 import FileUploadInput from "./_components/file-upload-input";
 import { getProcessedAudio } from "./_lib/audioUtils";
 import AnalysisWorkerAdapter from "./_lib/worker-adapter";
@@ -161,49 +155,6 @@ export default function AnalyzerScreen() {
         and may produce wrong results from time to time.
       </InfoCard>
     </div>
-  );
-}
-
-function DataCard(props: {
-  mdiIconPath: string;
-  title: string;
-  subTitle: string;
-}) {
-  const [hasCopied, setHasCopied] = useState(false);
-
-  // Might error out in debug due to insecure origin settings, should be fine in production
-  function handleCopyClicked() {
-    navigator.clipboard.writeText(props.subTitle).then(() => {
-      setHasCopied(true);
-      setTimeout(() => {
-        setHasCopied(false);
-      }, 2000);
-    });
-  }
-
-  return (
-    <motion.div
-      initial={{ opacity: 0, scale: 0.95, translateY: "16px" }}
-      animate={{
-        opacity: 1,
-        scale: 1,
-        translateY: "0px",
-        transition: { duration: 0.8, ease: [0, 0.7, 0.2, 1.0] },
-      }}
-      className={`flex flex-col gap-3 bg-popover p-4 rounded-lg text-card-foreground group`}
-    >
-      <div className="flex items-center gap-2 font-semibold">
-        <Icon path={props.mdiIconPath} size={1} />
-        <span className="sm:text-lg grow">{props.title}</span>
-        <div
-          onClick={!hasCopied ? handleCopyClicked : () => {}}
-          className="opacity-0 group-hover:opacity-100 text-muted-foreground cursor-pointer"
-        >
-          <Icon path={hasCopied ? mdiCheck : mdiContentCopy} size={0.8} />
-        </div>
-      </div>
-      <span>{props.subTitle}</span>
-    </motion.div>
   );
 }
 
